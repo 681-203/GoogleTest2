d93daa6da7b18c22ba130af9f23c622f47140c13
Atsuya-Kamekawa <sunrise285_yagp@icloud.com>
2024-11-16 18:27:22 +0900

    initial commit

---
+++ GoogleTest2/MyPageReplyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPageReplyViewController.swift
+//  GoogleTest2
+//
+//  Created by 亀川敦躍 on 2024/11/09.
+//
+
+import UIKit
+
+class MyPageReplyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
